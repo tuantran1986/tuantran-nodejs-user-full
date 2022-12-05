a4b75139ebc29fd5ef114d17a135354c7903d52b
tuanta <tuantran.kum@gmail.com>
2022-12-05 21:24:34 +0700

    bai 13-2- RES.LOCALS truyen du lieu giua cac MIDDLEWARE

--- routers/testMiddleWare.js
+++ routers/testMiddleWare.js
@@ -5,20 +5,25 @@ const testMiddleWareRouter = express.Router();
 // 
 const middleWare1 = (req, res, next) => {
     console.log('middleWare 1 is running, NEXT() để chạy middleWare2');
+    res.locals.dataMiddleWare1 = 'data - middleware - 1';
     next();
 }
 const middleWare2 = (req, res, next) => {
     console.log('middleWare 2 is running, NEXT() để chạy middleWare3');
+    res.locals.dataMiddleWare2 = 'data - middleware - 2';
     next();
 }
 const middleWare3 = (req, res, next) => {
     console.log('middleWare 3 is running, NEXT() để chạy RES.SEND');
+    res.locals.dataMiddleWare3 = 'data - middleware - 3';
+
     next();
 }
 
 
 // 
 testMiddleWareRouter.get('/', middleWare1, middleWare2, middleWare3, (req, res, next) => {
+    console.log('TRUYEN DATA giua cac MIDDLEWARE = RES.LOCALS :', res.locals);
     res.send('test MiddleWare : xem THỨ TỰ CHẠY MDW - trong CONSOLE LOG - BACKEND');
 })
 
